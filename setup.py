c91416e52c4a40a1518bae5ae7e8c35f3ed2a660
shimanekb <brandon.j.shimanek@gmail.com>
2019-03-06 19:59:25 -0600

    setup and tox

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Chess',
+      version='1.0',
+      description='Chess game',
+      author='Brandon Shimanek',
+      url='Na',
+      packages=[],
+      )
