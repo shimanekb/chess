c02756d5cab2b2c591b9d0446eb7e21cbb4d1441
shimanekb <brandon.j.shimanek@gmail.com>
2019-03-09 16:05:34 -0600

    quit test done

--- chess/app/controller/game_controller.py
+++ chess/app/controller/game_controller.py
@@ -3,6 +3,9 @@ Module for two player game of chess.
 """
 
 
+QUIT = 'Q'
+
+
 def play_game():
     """Play two player game of chess.
 
@@ -10,17 +13,34 @@ def play_game():
     -------
     None
     """
-    raise NotImplementedError()
+    while True:
+        display_board()
+        user_input = ask_move()
+
+        if user_input.upper() == QUIT:
+            break
 
 
-def quit_game():
-    """Quits current game of chess.
+def display_board():
+    """Displays current chess board state
 
     Returns
     -------
     None
     """
-    raise NotImplementedError()
+    # does nothing
+
+
+def ask_move():
+    """Ask user for move to make.
+
+    Returns
+    -------
+    str
+        User movement input.
+    """
+    user_input = input('Please make a move (example a5 a6) or quit (Q):')
+    return user_input
 
 
 def move_piece(positionFrom, positionTo):
@@ -31,10 +51,28 @@ def move_piece(positionFrom, positionTo):
     Parameters
     ----------
     positionFrom : Position
+        Position to move piece from.
     positionTo : Position
+        Position to move piece to.
 
     Returns
     -------
     None
     """
     raise NotImplementedError()
+
+
+def capture_piece(position):
+    """
+    Capture piece if one is at given position.
+
+    Parameters
+    ----------
+    position : Position
+
+    Returns
+    -------
+    None
+    """
+    raise NotImplementedError()
+
--- tests/acceptance/test_two_player_game.py
+++ tests/acceptance/test_two_player_game.py
@@ -1,9 +1,36 @@
 """
 Acceptance tests for two player game of chess.
 """
+import pytest
+from chess.app.controller import game_controller
 
 
-def test_movement():
+@pytest.fixture()
+def feed_input(monkeypatch):
+    def _feed_input(fed_input):
+        monkeypatch.setattr('builtins.input', lambda x: fed_input)
+
+    return _feed_input
+
+
+def test_movement(capsys):
+    game_controller.play_game()
+    board = capsys.readouterr()
+    print('LOOK\n')
+    print(board)
+
+    assert False
+
+
+def test_movement_retry_bad_input_not_rank():
+    assert False
+
+
+def test_movement_retry_bad_input_not_file():
+    assert False
+
+
+def test_movement_retry_bad_input_no_space():
     assert False
 
 
@@ -11,5 +38,7 @@ def test_capture():
     assert False
 
 
-def test_quit():
-    assert False
+@pytest.mark.timeout(1)
+def test_quit(feed_input):
+    feed_input('Q')
+    game_controller.play_game()
