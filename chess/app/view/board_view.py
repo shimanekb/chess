68b42f9a0b77690636fc1b64c372a91a236c68d3
shimanekb <brandon.j.shimanek@gmail.com>
2019-03-15 21:42:27 -0500

    added board view

---
+++ chess/app/view/board_view.py
@@ -0,0 +1,61 @@
+"""
+Module for board related views.
+"""
+from chess.board import Position
+
+
+def display(board):
+    """ Displays board to standard out.
+
+    Parameters
+    ----------
+    board : chess.board.Board
+        Board to display
+
+    Returns
+    -------
+    None
+    """
+    board_str = ''
+    for rank in range(8, 0, -1):
+        board_str += _generate_rank_line()
+        board_str += _build_rank_str(board, rank)
+
+    board_str += _generate_rank_line()
+    board_str += '     a     b     c     d    e     f     g     h'
+    print(board_str)
+
+
+def _build_rank_str(board, rank):
+    """ Builds string for a given rank (row).
+
+    Parameters
+    ----------
+    board : chess.board.Board
+        Board to display
+    rank : int
+        Rank to build string for.
+
+    Returns
+    -------
+    str
+        row representation for a given rank.
+    """
+    rank_str = '%s |' % rank
+    for file in range(ord('a'), ord('h') + 1):
+        position = Position('%s%s' % (chr(file), rank))
+        piece = board.get_piece(position)
+        if piece is None:
+            rank_str += '     '
+        else:
+            rank_str += '{:^5}'.format(piece.symbol)
+
+        rank_str += '|'
+
+    rank_str += '\n'
+
+    return rank_str
+
+
+def _generate_rank_line():
+    return '  -------------------------------------------------\n'
---
+++ tests/unit/app/test_board_view.py
@@ -0,0 +1,37 @@
+"""
+Unit tests for board_view
+"""
+from chess.board import Board
+from chess.app.view import board_view
+
+
+def test_display_printed_board(capsys):
+    # Given
+    expected_board_display = \
+        '  -------------------------------------------------\n'\
+        '8 | BR  | BK  | BB  | BQ  | BKi | BB  | BK  | BR  |\n'\
+        '  -------------------------------------------------\n'\
+        '7 | BP  | BP  | BP  | BP  | BP  | BP  | BP  | BP  |\n'\
+        '  -------------------------------------------------\n'\
+        '6 |     |     |     |     |     |     |     |     |\n'\
+        '  -------------------------------------------------\n'\
+        '5 |     |     |     |     |     |     |     |     |\n'\
+        '  -------------------------------------------------\n'\
+        '4 |     |     |     |     |     |     |     |     |\n'\
+        '  -------------------------------------------------\n'\
+        '3 |     |     |     |     |     |     |     |     |\n'\
+        '  -------------------------------------------------\n'\
+        '2 | WP  | WP  | WP  | WP  | WP  | WP  | WP  | WP  |\n'\
+        '  -------------------------------------------------\n'\
+        '1 | WR  | WK  | WB  | WQ  | WKi | WB  | WK  | WR  |\n' \
+        '  -------------------------------------------------\n'\
+        '     a     b     c     d    e     f     g     h'
+    board = Board()
+
+    # When
+    board_view.display(board)
+
+    # Then
+    outerr = capsys.readouterr()
+
+    assert expected_board_display in outerr.out
