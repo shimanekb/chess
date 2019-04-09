59486fe215f58fc3848a431dce8fea8925e2d20c
shimanekb <brandon.j.shimanek@gmail.com>
2019-04-08 21:45:59 -0500

    added more acceptance tests for and diag

--- tests/acceptance/test_pawn_movement.py
+++ tests/acceptance/test_pawn_movement.py
@@ -1,11 +1,12 @@
 """
 Acceptance tests for pawn movement.
 """
+from chess.set.box import Color
 from chess.set.table import Board
 from chess.set.table import Position
 
 
-def test_move_pawn_forward_valid_movement():
+def test_move_white_pawn_forward_valid_movement():
     # Given
     board = Board()
     pawn_position_from = Position('a2')
@@ -20,6 +21,68 @@ def test_move_pawn_forward_valid_movement():
     # Then
     assert board.get_piece(pawn_position_to).symbol == 'P'
 
+
+def test_move_black_pawn_forward_valid_movement():
+    # Given
+    board = Board()
+    pawn_position_from = Position('b7')
+    pawn_position_to = Position('b6')
+
+    assert board.get_piece(pawn_position_to) is None
+    assert board.get_piece(pawn_position_from).symbol == 'P'
+
+    # When
+    board.move_piece(pawn_position_from, pawn_position_to)
+
+    # Then
+    assert board.get_piece(pawn_position_to).symbol == 'P'
+
+
+def test_move_white_pawn_diagonal_capture_valid_movement():
+    # Given
+    board = Board()
+    board.move_piece(Position('a2'), Position('a4'))
+    board.move_piece(Position('b7'), Position('b5'))
+
+    pawn_position_from = Position('a4')
+    pawn_position_to = Position('b5')
+
+    assert board.get_piece(pawn_position_to).color == Color.BLACK
+    assert board.get_piece(pawn_position_to).symbol == 'P'
+
+    assert board.get_piece(pawn_position_from).color == Color.WHITE
+    assert board.get_piece(pawn_position_from).symbol == 'P'
+
+    # When
+    board.move_piece(pawn_position_from, pawn_position_to)
+
+    # Then
+    assert board.get_piece(pawn_position_to).color == Color.WHITE
+    assert board.get_piece(pawn_position_to).symbol == 'P'
+
+
+def test_move_black_pawn_diagonal_capture_valid_movement():
+    # Given
+    board = Board()
+    board.move_piece(Position('a2'), Position('a4'))
+    board.move_piece(Position('b7'), Position('b5'))
+    board.move_piece(Position('a1'), Position('a2'))
+
+    pawn_position_from = Position('b5')
+    pawn_position_to = Position('a4')
+
+    assert board.get_piece(pawn_position_to).color == Color.WHITE
+    assert board.get_piece(pawn_position_to).symbol == 'P'
+
+    assert board.get_piece(pawn_position_from).color == Color.BLACK
+    assert board.get_piece(pawn_position_from).symbol == 'P'
+
+    # When
+    board.move_piece(pawn_position_from, pawn_position_to)
+
+    # Then
+    assert board.get_piece(pawn_position_to).color == Color.BLACK
+    assert board.get_piece(pawn_position_to).symbol == 'P'
 
 """
 def test_move_pawn_by_two_valid_movement():
